3b3f66c06b278aea670da4d9c029c20ed73fa1c8
flugdrachen <flugdrachen@users.noreply.github.com>
2022-11-03 17:36:29 +0100

    Copy usual Process Lower Star Algorithm

--- src/Algorithms/ConformingGradient.py
+++ src/Algorithms/ConformingGradient.py
@@ -1,26 +1,33 @@
 from .PriorityQueue import PriorityQueue
-from .ProcessLowerStars import lower_star
 
 import numpy as np
 import timeit
 from collections import Counter, deque
 
-def construct_labels(lower_star, edges_dict, faces_dict, labels_dict):
-    labels = {'edges': {}, 'faces': {}}
+def lower_conforming_star(vertex, edges_dict, faces_dict):
+    # x a vertex
+    # data star is list of heights and indices of all cells having x as subset
+    
+    lower_star = {}
+    lower_star['vertex'] = {vertex.index : vertex.fun_val}
+    lower_star['edges'] = {}
+    lower_star['faces'] = {}
+    
+    for edge_ind in vertex.star["E"]:
+        if edges_dict[edge_ind].fun_val[0] == vertex.fun_val:
+            lower_star['edges'][edge_ind] = edges_dict[edge_ind].fun_val
+            
+    for face_ind in vertex.star["F"]:
+        if faces_dict[face_ind].fun_val[0] == vertex.fun_val:
+            lower_star['faces'][face_ind] = faces_dict[face_ind].fun_val
+    
+    # sort edges and cells: biggest/highest at the end
+    lower_star['edges'] = {k: v for k, v in sorted(lower_star['edges'].items(), key=lambda item: item[1])[::-1]}
+    lower_star['faces'] = {k: v for k, v in sorted(lower_star['faces'].items(), key=lambda item: item[1])[::-1]}
 
-    for key in lower_star['edges'].keys():
-        labels['edges'][key] = set()
-        for i in edges_dict[key].indices:
-            labels['edges'][key].add(labels_dict[i])
+    return lower_star
 
-    for key in lower_star['faces'].keys():
-        labels['faces'][key] = set()
-        for i in edges_dict[key].indices:
-            labels['faces'][key].add(labels_dict[i])
-
-    return labels
-
-def num_unpaired_conforming_faces(face, PQzero, edges_dict, faces_dict, star_labels):
+def num_unpaired_conforming_faces(face, PQzero, edges_dict, faces_dict):
     # checks number of faces in PQzero ( should be equal to number of unpaired faces in lower star, 
     # since all edges from lower star go to PQzero or are paired/ added to C and removed from PQzero)
     number = 0
@@ -30,42 +37,33 @@ def num_unpaired_conforming_faces(face, PQzero, edges_dict, faces_dict, star_lab
                 number+=1
     return number
 
-def pair(face, PQzero, edges_dict, faces_dict, star_labels):
+def pair(face, PQzero, edges_dict, faces_dict):
     for key in PQzero.keys():
         if len(edges_dict[key].indices) ==2:
             if (edges_dict[key].indices).issubset(faces_dict[face].indices):
                 return key, PQzero.pop_key(key)
 
-def ConformingGradient(vertices_dict, edges_dict, faces_dict, labels_dict, C, V12, V23):
+def ConformingGradient(vertices_dict, edges_dict, faces_dict, C, V12, V23):
     start_eff = timeit.default_timer()
     
     for vertex in vertices_dict.values():
-        lowerStar = lower_star(vertex, edges_dict, faces_dict)
-
-        if len(lowerStar['edges']) == 0:
-            C[0].add(vertex.index)
-        else:
-            star_labels = construct_labels(lowerStar, edges_dict, faces_dict, labels_dict)
+        lowerStar = lower_conforming_star(vertex, edges_dict, faces_dict)
         
-            label_set = {labels_dict[vertex.index]}
+        if (len(lowerStar['edges'])+len(lowerStar['faces'])) == 0:
+            C[0].add(vertex.index)
             
-            conformingEdges = {key for key in lower_star['edges'].keys() if star_labels['edges'][key] == label_set}
-
-            if len(conformingEdges) == 0:
-                C[0].add(vertex.index)
-            else:
-                key = conformingEdges.popitem()
-                delta_key, delta_value = lowerStar['edges'].pop(key)
-                V12[vertex.index] = delta_key
-
+        else:
             PQzero = PriorityQueue()
             PQone = PriorityQueue()
+
+            delta_key, delta_value = lowerStar['edges'].popitem()
+            V12[vertex.index] = delta_key
             
             for key, value in lowerStar['edges'].items():
                 PQzero.insert(tuple((key, value)))
             
             for key, value in lowerStar['faces'].items():
-                if (num_unpaired_conforming_faces(key, PQzero, edges_dict, faces_dict, star_labels) == 1 and value > delta_value):
+                if (num_unpaired_conforming_faces(key, PQzero, edges_dict, faces_dict) == 1 and value > delta_value):
                     PQone.insert(tuple((key, value)))
 
             while (PQone.notEmpty() or PQzero.notEmpty()):
