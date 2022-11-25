931c349e9e6b103c46a4afb0a947ebf6628a4437
Jan Philipp Bullenkamp <jpbullenkamp@aol.de>
2022-11-25 18:55:07 +0100

    plots detailed whole morse complex now as overlay file might need to exclude crit points and write to another file, as this prevents ugly transparent points

--- src/PlotData/write_overlay_ply_files.py
+++ src/PlotData/write_overlay_ply_files.py
@@ -82,7 +82,7 @@ def write_face(file, face, vert_dict, color=[0,0,0], alpha=None):
     else:
         file.write(str(color[0]) + " " + str(color[1]) + " " + str(color[2]) + " " + str(alpha) + "\n")
     
-def write_collapsed_face(file, first_ind, second_ind, third_ind, color=[0,0,0], alpha=None):
+def write_real_face(file, first_ind, second_ind, third_ind, color=[0,0,0], alpha=None):
     file.write("3 " + str(first_ind) + " " + str(second_ind) + " " + str(third_ind) + " ")
     if alpha == None:
         file.write(str(color[0]) + " " + str(color[1]) + " " + str(color[2]) + "\n")
@@ -254,7 +254,7 @@ def write_MSComplex_detailed_overlay_ply_file(MorseCpx, vert_dict, edge_dict, fa
                 if nb == 1:
                     # remove first and last elt, as they are in Crit already
                     path = MorseCpx.CritEdges[saddle].paths[mini][1:-1]
-                    # first is face, second edge, so now we start with an edge
+                    # first is edge, second vert, so now we start with an vert
                     count = 0
                     for elt in path:
                         if count%2 == 0:
@@ -268,7 +268,7 @@ def write_MSComplex_detailed_overlay_ply_file(MorseCpx, vert_dict, edge_dict, fa
                     for i in range(2):
                         # remove first and last elt, as they are in Crit already
                         path = MorseCpx.CritEdges[saddle].paths[mini][i][1:-1]
-                        # first is face, second edge, so now we start with an edge
+                        # first is edge, second vert, so now we start with an vert
                         count = 0
                         for elt in path:
                             if count%2 == 0:
@@ -277,32 +277,51 @@ def write_MSComplex_detailed_overlay_ply_file(MorseCpx, vert_dict, edge_dict, fa
                                 path_edges.add(elt)
                                 path_segment_edge_vert.add(elt)
                             count+=1
-        nb_points = len(MorseCpx.CritVertices) + len(MorseCpx.CritEdges) + len(MorseCpx.CritFaces) + len(path_vert) + len(path_edges) + len(path_faces)
+        nb_points = 3*len(MorseCpx.CritVertices) + 4*len(MorseCpx.CritEdges) + 4*len(MorseCpx.CritFaces) + len(path_vert) + len(path_edges) + len(path_faces) + 3*len(path_segment_edge_vert)
         
-        nb_faces = len(path_segment_face_edge) #+ len(path_segment_edge_vert)
+        nb_faces = len(path_segment_face_edge) + len(MorseCpx.CritEdges) + len(MorseCpx.CritFaces) + len(path_segment_edge_vert) + len(MorseCpx.CritVertices)
         extra_points = set([face for (face,saddle) in path_segment_face_edge])
 
         write_header(f, nb_points+len(extra_points), nb_faces)
         
         transfer_edges = {}
         transfer_faces = {}
+        crit_edge_transfer = {}
+        crit_face_transfer = {}
+        crit_vert_transfer = {}
+        path_edge_transfer = {}
         
         v_index = 0
         
         '''Write Vertices'''
         for vert in MorseCpx.CritVertices.values():
-            write_vertex(f, vert, vert_dict, color=[255,0,0], alpha=0)
-            v_index+=1
+            crit_vert_transfer[vert.index] = v_index # vert can be chosen as v_ind, v_ind+1, v_ind+2
+            for i in range(3):
+                write_vertex(f, vert, vert_dict, color=[255,0,0], alpha=255)
+                v_index+=1
         for ed in MorseCpx.CritEdges.values():
-            write_edge(f, ed, vert_dict, color=[0,255,0], alpha=0)
+            write_edge(f, ed, vert_dict, color=color_paths, alpha=0)
             transfer_edges[ed.index] = v_index
             v_index+=1
+            # for writing crit edge in green:
+            crit_edge_transfer[ed.index] = v_index # edge can be chosen as v_ind, v_ind+1, v_ind+2
+            for vert in ed.indices:
+                write_vertex(f, vert_dict[vert], vert_dict, color=[0,255,0], alpha=0)
+                v_index+=1
+            write_vertex(f, vert_dict[next(iter(ed.indices))], vert_dict, color=[0,255,0], alpha=0)
+            v_index+=1
+            
         for fa in MorseCpx.CritFaces.values():
-            write_face(f, fa, vert_dict, color=[0,0,255], alpha=0)
+            write_face(f, fa, vert_dict, color=color_paths, alpha=0)
             transfer_faces[fa.index] = v_index
             v_index+=1
             if fa.index in extra_points:
-                write_face(f, fa, vert_dict, color=[0,0,255], alpha=0)
+                write_face(f, fa, vert_dict, color=color_paths, alpha=0)
+                v_index+=1
+            # for writing crit face in blue:
+            crit_face_transfer[fa.index] = v_index # face can be chosen as v_ind, v_ind+1, v_ind+2
+            for vert in fa.indices:
+                write_vertex(f, vert_dict[vert], vert_dict, color=[0,0,255], alpha=0)
                 v_index+=1
 
         for ind in path_vert:
@@ -312,6 +331,14 @@ def write_MSComplex_detailed_overlay_ply_file(MorseCpx, vert_dict, edge_dict, fa
             write_edge(f, edge_dict[ind], vert_dict, color=color_paths, alpha=0)
             transfer_edges[ind] = v_index
             v_index+=1
+            if ind in path_segment_edge_vert:
+                path_edge_transfer[ind] = v_index # edge can be chosen as v_ind, v_ind+1, v_ind+2
+                for vert in edge_dict[ind].indices:
+                    write_vertex(f, vert_dict[vert], vert_dict, color=color_paths, alpha=0)
+                    v_index+=1
+                write_vertex(f, vert_dict[next(iter(edge_dict[ind].indices))], vert_dict, color=color_paths, alpha=0)
+                v_index+=1
+                
         for ind in path_faces:
             write_face(f, face_dict[ind], vert_dict, color=color_paths, alpha=0)
             transfer_faces[ind] = v_index
@@ -322,7 +349,19 @@ def write_MSComplex_detailed_overlay_ply_file(MorseCpx, vert_dict, edge_dict, fa
             
         '''Write Faces (that will be visualized edges)'''
         for (face_ind, edge_ind) in path_segment_face_edge:
-            write_collapsed_face(f, transfer_faces[face_ind], transfer_faces[face_ind]+1, transfer_edges[edge_ind], color=color_paths, alpha=255)
+            write_real_face(f, transfer_faces[face_ind], transfer_faces[face_ind]+1, transfer_edges[edge_ind], color=color_paths, alpha=255)
+            
+        for edge_ind, first_index in path_edge_transfer.items():
+            write_real_face(f, first_index, first_index+1, first_index+2, color=color_paths, alpha=255)
+        
+        for vert_ind, first_index in crit_vert_transfer.items():
+            write_real_face(f, first_index, first_index+1, first_index+2, color=[255,0,0], alpha=255)
+        
+        for edge_ind, first_index in crit_edge_transfer.items():
+            write_real_face(f, first_index, first_index+1, first_index+2, color=[0,255,0], alpha=255)
+        
+        for face_ind, first_index in crit_face_transfer.items():
+            write_real_face(f, first_index, first_index+1, first_index+2, color=[0,0,255], alpha=255)
         
          
 
