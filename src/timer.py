55586cbf9a84da76d850ebcf8a9884be2f8be58d
JanPhilipp-Bullenkamp <jpbullenkamp@aol.de>
2023-03-04 23:44:10 +0100

    add flag to timer function

--- src/timer.py
+++ src/timer.py
@@ -1,13 +1,14 @@
 
 import time
-
-def timed(function, flag=True):
-    def wrapper(*args, **kwargs):
-        before = time.time()
-        return_value = function(*args, **kwargs)
-        after = time.time()
-        fname = function.__name__
-        if flag:
-            print(f"{fname} took {(after-before):.5f} seconds to execute!")
-        return return_value
-    return wrapper
+def timed(flag: bool = True):
+    def time_fct(function):
+        def wrapper(*args, **kwargs):
+            before = time.time()
+            return_value = function(*args, **kwargs)
+            after = time.time()
+            fname = function.__name__
+            if flag:
+                print(f"{fname} took {(after-before):.5f} seconds to execute!")
+            return return_value
+        return wrapper
+    return time_fct
