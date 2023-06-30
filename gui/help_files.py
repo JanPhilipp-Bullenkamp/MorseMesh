38555e177d9da11d729854441e16750bad208880
Jan Philipp Bullenkamp <jpbullenkamp@aol.de>
2023-06-30 13:50:24 +0200

    added mainwindow.ui to resources and small restructure in folder structure

--- gui/help_files.py
+++ gui/help_files.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 
-from .resources.quick_guide import Ui_dialog_quickguide
-from .resources.info_contact import Ui_dialog_info_contact
+from quick_guide import Ui_dialog_quickguide
+from info_contact import Ui_dialog_info_contact
 
 
 class QuickGuide(QtWidgets.QMainWindow):
